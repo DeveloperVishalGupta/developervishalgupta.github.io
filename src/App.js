67be9350be3d1dc626645a2eec6794fc13123ace
Vishal Gupta <vishalguptaramasare@gmail.com>
2026-05-10 22:57:28 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -13,20 +13,10 @@ import greenStrip from './images/greenStrip.png';
 import VishalGupta from './images/vishalGupta.png';
 import { faLaptopCode, faPencilSquare, faCircleCheck, } from '@fortawesome/free-solid-svg-icons';
 import { faInstagram, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
-import { Helmet } from "react-helmet";
 
 function App() {
     return (
         <>
-            <Helmet>
-                <title>Dynamic Page Title</title>
-                <meta name="description" content="Energetic frontend developer with 3 year 5 months of experience in creating responsive and user-friendly web applications, from planning to launch. Skilled in post-production support and resolving UI/UX defects for various business processes. Proficient in HTML, CSS, JavaScript, and modern frameworks like React JS and Next.js." />
-                <meta property="og:title" content="Frontend Developer | Web Developer | Building Responsive and Interactive Interfaces" />
-                <meta property="og:description" content="Energetic Frontend Developer with 3+ years of experience in creating responsive and user-friendly web applications using HTML, CSS, JavaScript, React.js, and Next.js. Skilled in UI/UX optimization and post-production support." />
-                <meta property="og:image" content="./images/vishalGupta.png" />
-                <meta property="og:image:alt" content="Portfolio of Vishal Gupta - Frontend Developer" />
-                <meta property="og:url" content="https://developervishalgupta.github.io/vishalportfolio/" />
-            </Helmet>
             <Header />
             <Home greenStrip={greenStrip} VG={VishalGupta} circelCheck={faCircleCheck} intsaIcon={faInstagram}
                 Github={faGithub} linkedIn={faLinkedinIn} />
